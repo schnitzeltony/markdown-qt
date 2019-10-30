50d30346e2da4ce167d96ff0cf239b236e6b1b83
Andreas Müller <schnitzeltony@gmail.com>
2019-10-30 21:34:10 +0100

    Do not start conversion if cmark-string is empty
    Sundown conversion crashes - is it my implementation?
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/markdown-qt.cpp
+++ src/markdown-qt.cpp
@@ -28,51 +28,53 @@ QString CMarkDownQt::stringToHtml(TreatParam paramAs, const QString &strIn, Conv
 {
     QByteArray tmpData;
     switch(paramAs) {
-    case AsString:
-        tmpData = strIn.toUtf8();
-        break;
-    case AsFilename: {
-        QFile cmFile(strIn);
-        if(cmFile.exists() && cmFile.open(QFile::ReadOnly | QFile::Unbuffered)) {
-            tmpData = cmFile.readAll();
-            cmFile.close();
+        case AsString:
+            tmpData = strIn.toUtf8();
+            break;
+        case AsFilename: {
+            QFile cmFile(strIn);
+            if(cmFile.exists() && cmFile.open(QFile::ReadOnly | QFile::Unbuffered)) {
+                tmpData = cmFile.readAll();
+                cmFile.close();
+            }
+            break;
         }
-        break;
-    }
     }
     QString strHtml;
-    switch(convertType) {
-        case ConvertCmark:
-            strHtml = QString::fromUtf8(cmark_markdown_to_html(tmpData.constData(), size_t(tmpData.size()), CMARK_OPT_DEFAULT));
-            break;
-        case ConvertSundown:
-        {
-            struct sd_callbacks callbacks;
-            struct html_renderopt options;
-            struct sd_markdown *markdown;
-            struct buf *sdInputBuf, *sdOutputBuf;
-            // fill input buffer (size is constant -> realloc size is fill size)
-            sdInputBuf = bufnew(size_t(tmpData.size()));
-            bufput(sdInputBuf, tmpData.constData(), size_t(tmpData.size()));
-            // out (realloc size taken from example)
-            sdOutputBuf = bufnew(size_t(64));
-            sdhtml_renderer(&callbacks, &options, 0);
-            // select all extensions for now
-            markdown = sd_markdown_new(
-                        MKDEXT_NO_INTRA_EMPHASIS | MKDEXT_TABLES | MKDEXT_FENCED_CODE | MKDEXT_AUTOLINK | MKDEXT_STRIKETHROUGH | MKDEXT_SPACE_HEADERS | MKDEXT_SUPERSCRIPT | MKDEXT_LAX_SPACING,
-                        16, &callbacks, &options);
-            // convert
-            sd_markdown_render(sdOutputBuf, sdInputBuf->data, sdInputBuf->size, markdown);
-            sd_markdown_free(markdown);
+    if(!tmpData.isEmpty()) {
+        switch(convertType) {
+            case ConvertCmark:
+                strHtml = QString::fromUtf8(cmark_markdown_to_html(tmpData.constData(), size_t(tmpData.size()), CMARK_OPT_DEFAULT));
+                break;
+            case ConvertSundown:
+            {
+                struct sd_callbacks callbacks;
+                struct html_renderopt options;
+                struct sd_markdown *markdown;
+                struct buf *sdInputBuf, *sdOutputBuf;
+                // fill input buffer (size is constant -> realloc size is fill size)
+                sdInputBuf = bufnew(size_t(tmpData.size()));
+                bufput(sdInputBuf, tmpData.constData(), size_t(tmpData.size()));
+                // out (realloc size taken from example)
+                sdOutputBuf = bufnew(size_t(64));
+                sdhtml_renderer(&callbacks, &options, 0);
+                // select all extensions for now
+                markdown = sd_markdown_new(
+                            MKDEXT_NO_INTRA_EMPHASIS | MKDEXT_TABLES | MKDEXT_FENCED_CODE | MKDEXT_AUTOLINK | MKDEXT_STRIKETHROUGH | MKDEXT_SPACE_HEADERS | MKDEXT_SUPERSCRIPT | MKDEXT_LAX_SPACING,
+                            16, &callbacks, &options);
+                // convert
+                sd_markdown_render(sdOutputBuf, sdInputBuf->data, sdInputBuf->size, markdown);
+                sd_markdown_free(markdown);
 
-            strHtml = QString::fromUtf8(reinterpret_cast<char*>(sdOutputBuf->data), int(sdOutputBuf->size));
+                strHtml = QString::fromUtf8(reinterpret_cast<char*>(sdOutputBuf->data), int(sdOutputBuf->size));
 
-            /* cleanup */
-            bufrelease(sdInputBuf);
-            bufrelease(sdOutputBuf);
+                /* cleanup */
+                bufrelease(sdInputBuf);
+                bufrelease(sdOutputBuf);
 
 
-            break;
+                break;
+            }
         }
     }
 
