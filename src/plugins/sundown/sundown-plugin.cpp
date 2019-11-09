3eebd1f546c50a5a831a76c46307e0fdf0f777ce
Andreas Müller <schnitzeltony@gmail.com>
2019-11-09 22:05:45 +0100

    sundown: do no conversion with empty input to avoid assertion
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/plugins/sundown/sundown-plugin.cpp
+++ src/plugins/sundown/sundown-plugin.cpp
@@ -32,29 +32,31 @@ bool CSundownPlugin::convert(ConvertType convertType, const QByteArray strIn, QB
 QByteArray CSundownPlugin::convertToHtml(QByteArray strMarkDownUtf8)
 {
     QByteArray strHtml;
-    struct sd_callbacks callbacks;
-    struct html_renderopt options;
-    struct sd_markdown *markdown;
-    struct buf *sdInputBuf, *sdOutputBuf;
-    // fill input buffer (size is constant -> realloc size is fill size)
-    sdInputBuf = bufnew(size_t(strMarkDownUtf8.size()));
-    bufput(sdInputBuf, strMarkDownUtf8, size_t(strMarkDownUtf8.size()));
-    // out (realloc size taken from example)
-    sdOutputBuf = bufnew(size_t(64));
-    sdhtml_renderer(&callbacks, &options, 0);
-    // select all extensions for now
-    markdown = sd_markdown_new(
-                MKDEXT_NO_INTRA_EMPHASIS | MKDEXT_TABLES | MKDEXT_FENCED_CODE | MKDEXT_AUTOLINK | MKDEXT_STRIKETHROUGH | MKDEXT_SPACE_HEADERS | MKDEXT_SUPERSCRIPT | MKDEXT_LAX_SPACING,
-                16, &callbacks, &options);
-    // convert
-    sd_markdown_render(sdOutputBuf, sdInputBuf->data, sdInputBuf->size, markdown);
-    sd_markdown_free(markdown);
+    if(strMarkDownUtf8.size() > 0)
+    {
+        struct sd_callbacks callbacks;
+        struct html_renderopt options;
+        struct sd_markdown *markdown;
+        struct buf *sdInputBuf, *sdOutputBuf;
+        // fill input buffer (size is constant -> realloc size is fill size)
+        sdInputBuf = bufnew(size_t(strMarkDownUtf8.size()));
+        bufput(sdInputBuf, strMarkDownUtf8, size_t(strMarkDownUtf8.size()));
+        // out (realloc size taken from example)
+        sdOutputBuf = bufnew(size_t(64));
+        sdhtml_renderer(&callbacks, &options, 0);
+        // select all extensions for now
+        markdown = sd_markdown_new(
+                    MKDEXT_NO_INTRA_EMPHASIS | MKDEXT_TABLES | MKDEXT_FENCED_CODE | MKDEXT_AUTOLINK | MKDEXT_STRIKETHROUGH | MKDEXT_SPACE_HEADERS | MKDEXT_SUPERSCRIPT | MKDEXT_LAX_SPACING,
+                    16, &callbacks, &options);
+        // convert
+        sd_markdown_render(sdOutputBuf, sdInputBuf->data, sdInputBuf->size, markdown);
+        sd_markdown_free(markdown);
 
-    strHtml = QByteArray(reinterpret_cast<char*>(sdOutputBuf->data), int(sdOutputBuf->size));
+        strHtml = QByteArray(reinterpret_cast<char*>(sdOutputBuf->data), int(sdOutputBuf->size));
 
-    /* cleanup */
-    bufrelease(sdInputBuf);
-    bufrelease(sdOutputBuf);
-
+        /* cleanup */
+        bufrelease(sdInputBuf);
+        bufrelease(sdOutputBuf);
+    }
     return strHtml;
 }
