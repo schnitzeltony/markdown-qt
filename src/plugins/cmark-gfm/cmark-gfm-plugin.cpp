ab360dc044dd8b820e7a131839321465cf426f8c
Andreas Müller <schnitzeltony@gmail.com>
2019-11-11 23:38:09 +0100

    cmark-gfm-plugin: Make tasklists display as github does
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- src/plugins/cmark-gfm/cmark-gfm-plugin.cpp
+++ src/plugins/cmark-gfm/cmark-gfm-plugin.cpp
@@ -3,7 +3,8 @@
 #include "cmark-gfm-source/extensions/cmark-gfm-core-extensions.h"
 #include "cmark-gfm-source/src/html.h"
 #include "parser.h"
-#include <QStringList>
+#include "chtmlhelper.h"
+#include <QMapIterator>
 
 CMarkGfmPlugin::~CMarkGfmPlugin()
 {
@@ -34,7 +35,7 @@ bool CMarkGfmPlugin::convert(ConvertType convertType, const QByteArray strIn, QB
 
 QByteArray CMarkGfmPlugin::convertToHtml(QByteArray strMarkDownUtf8)
 {
-    int options = CMARK_OPT_UNSAFE | CMARK_OPT_LIBERAL_HTML_TAG | CMARK_OPT_GITHUB_PRE_LANG; /* TODO options for plugins */
+    int options = CMARK_OPT_UNSAFE | CMARK_OPT_LIBERAL_HTML_TAG | CMARK_OPT_GITHUB_PRE_LANG | CMARK_OPT_FULL_INFO_STRING; /* TODO options for plugins */
     cmark_gfm_core_extensions_ensure_registered();
     cmark_parser *parser = cmark_parser_new_with_mem(options, cmark_get_arena_mem_allocator());
     QByteArray strHtml;
@@ -57,6 +58,52 @@ QByteArray CMarkGfmPlugin::convertToHtml(QByteArray strMarkDownUtf8)
         cmark_node *document = cmark_parser_finish(parser);
         if(document) {
             strHtml = cmark_render_html_with_mem(document, options, parser->syntax_extensions, cmark_get_default_mem_allocator());
+            // some github specific post-processing
+            // Tasklist first lines
+            strHtml = strHtml.replace(
+                        "<ul>\n<li><input type=\"checkbox\"",
+                        "<ul class=\"contains-task-list\">\n<li class=\"task-list-item\"><input type=\"checkbox\"");
+            // Tasklist other lines
+            strHtml = strHtml.replace(
+                        "<li><input type=\"checkbox\"",
+                        "<li class=\"task-list-item\"><input type=\"checkbox\"");
+            // re-sort <p> for checklist text
+            // <li class="task-list-item"><input type="checkbox" checked="" disabled="" /><p>foo</p>
+            // turns into
+            // <p><li class="task-list-item"><input type="checkbox" checked="" disabled="" />foo</p>
+            CHtmlHelper htmlHelper;
+            if(htmlHelper.parse(strHtml)) {
+                const QMap<int, TagInfo>& tagMap = htmlHelper.GetTagMap();
+                int currSearchPos = 0;
+                do {
+                    currSearchPos = strHtml.indexOf("<input type=\"checkbox\"", currSearchPos);
+                    if(currSearchPos >= 0) {
+                        if(tagMap.contains(currSearchPos)) {
+                            auto iter = tagMap.find(currSearchPos);
+                            const TagInfo& tagInfo = iter.value();
+                            currSearchPos = tagInfo.endPos+1;
+                            iter++;
+                            if(iter != tagMap.constEnd()) {
+                                const TagInfo& tagInfoNext = iter.value();
+                                QByteArray nextTag = strHtml.mid(tagInfoNext.startPos, tagInfoNext.endPos+1 - tagInfoNext.startPos);
+                                if(nextTag == "<p>") {
+                                    currSearchPos = tagInfoNext.endPos+1;
+                                    QByteArray leftPart = strHtml.left(tagInfo.startPos);
+                                    QByteArray midPart = QByteArrayLiteral("<p>") + strHtml.mid(tagInfo.startPos, tagInfo.endPos+1 - tagInfo.startPos);
+                                    QByteArray rightPart = strHtml.right(strHtml.size()-tagInfoNext.endPos-1);
+                                    strHtml = leftPart + midPart + QByteArrayLiteral(" ") + rightPart;
+                                    // by adding a space all following tags are not matching anymore
+                                    htmlHelper.parse(strHtml, leftPart.size()+midPart.size());
+                                }
+                            }
+                        }
+                        else {
+                            // should not happen - but avoids deadlock
+                            currSearchPos++;
+                        }
+                    }
+                } while(currSearchPos >= 0 && currSearchPos < strHtml.count());
+            }
         }
         else {
             qWarning("CMARK no document node created");
